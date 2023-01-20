d341be6b84300593f0a0a9b142f967f1f3787ce6
kevserhuseynova <113387285+kevserhuseynova@users.noreply.github.com>
2023-01-20 12:18:31 +0000

    import ivy from ivy.func_wrapper import with_unsupported_dtypes from ivy.functional.frontends.tensorflow.func_wrapper import (     to_ivy_arrays_and_back,     handle_tf_dtype,     to_ivy_dtype, ) from ivy.functional.frontends.tensorflow.tensor import EagerTensor import ivy.functional.frontends.tensorflow as tf_frontend
    @to_ivy_arrays_and_back
    def size(input, out_type=ivy.int32, name=None):
        out_type = to_ivy_dtype(out_type)
        if out_type in ["int32", "int64"]:
            return ivy.array(ivy.size(input), dtype=out_type)
        else:
            return ivy.array(ivy.size(input), dtype="int64")

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -139,6 +139,15 @@ def shape_n(input, out_type=ivy.int32, name=None):
         return [ivy.array(ivy.shape(i), dtype="int64") for i in input]
 
 
+@to_ivy_arrays_and_back
+def size(input, out_type=ivy.int32, name=None):
+    out_type = to_ivy_dtype(out_type)
+    if out_type in ["int32", "int64"]:
+        return ivy.array(ivy.size(input), dtype=out_type)
+    else:
+        return ivy.array(ivy.size(input), dtype="int64")
+
+
 @with_unsupported_dtypes({"2.10.0 and below": ("float16", "bfloat16")}, "tensorflow")
 @handle_tf_dtype
 @to_ivy_arrays_and_back
